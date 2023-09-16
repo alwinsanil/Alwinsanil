75837906c62e2514639d93f9fcd79122d9e57f40
alwinsanil <alwinsanil@gmail.com>
2023-09-16 19:23:19 +0400

    inter-2

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <main className="w-full bg-gray-950 text-gray-300 px-4">
       <div className="max-w-screen-lg mx-auto lg:pt-10">
-        <NavBar />
+        {/* <NavBar /> */}
         <Banner />
         {/* <AboutMe /> */}
         <Experience />
