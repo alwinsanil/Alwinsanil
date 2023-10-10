6ccf2ec895b364b04d1282c177e17d9ed21c3321
alwinsanil <alwinsanil@gmail.com>
2023-10-10 11:29:53 +0400

    Oct10

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='pt-20 pl-30 pr-30 flex flex-row justify-center gap-3'>
+    <div className='invisible md:visible pt-20 pl-30 pr-30 flex flex-row justify-center gap-3'>
         <div className='flex items-center group'>
             <h3 className='text-2xl font-semibold relative overflow-hidden'>
                 <a href='#exp'>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -8,23 +8,23 @@ const Skills = () => {
     <div id='skills' className='wrapper'>
         <Title text='Skills' icon={<DiGoogleAnalytics />} />
         <div className='flex gap-6 flex-wrap'>
-            <SkillCard name='JavaScript' url='https://github.com/alwinsanil' />
-            <SkillCard name='Python' url='https://github.com/alwinsanil' />
-            <SkillCard name='SQL' url='https://github.com/alwinsanil' />
-            <SkillCard name='Reactjs' url='https://github.com/alwinsanil' />
-            <SkillCard name='Nextjs' url='https://github.com/alwinsanil' />
-            <SkillCard name='Advanced Excel' url='https://github.com/alwinsanil' />
-            <SkillCard name='C++' url='https://github.com/alwinsanil' />
-            <SkillCard name='C Programming' url='https://github.com/alwinsanil' />
-            <SkillCard name='Nodejs' url='https://github.com/alwinsanil' />
-            <SkillCard name='HTML5' url='https://github.com/alwinsanil' />
-            <SkillCard name='CSS' url='https://github.com/alwinsanil' />
+            <SkillCard name='JavaScript' url='https://www.javascript.com/' />
+            <SkillCard name='Python' url='https://www.w3schools.com/python/' />
+            <SkillCard name='SQL' url='https://www.w3schools.com/sql/' />
+            <SkillCard name='Reactjs' url='https://react.dev/' />
+            <SkillCard name='Nextjs' url='https://nextjs.org/' />
+            <SkillCard name='Advanced Excel' url='https://www.microsoft.com/en-us/microsoft-365/excel' />
+            <SkillCard name='C++' url='https://www.w3schools.com/cpp/' />
+            <SkillCard name='C Programming' url='https://www.w3schools.com/c/c_intro.php' />
+            <SkillCard name='Nodejs' url='https://nodejs.org/en' />
+            <SkillCard name='HTML5' url='https://www.w3schools.com/html/' />
+            <SkillCard name='CSS' url='https://www.w3schools.com/css/' />
             <SkillCard name='GitHub' url='https://github.com/alwinsanil' />
-            <SkillCard name='Git' url='https://github.com/alwinsanil' />
-            <SkillCard name='Tailwind CSS' url='https://github.com/alwinsanil' />
-            <SkillCard name='Databases' url='https://github.com/alwinsanil' />
-            <SkillCard name='Power BI' url='https://github.com/alwinsanil' />
-            <SkillCard name='Tableau' url='https://github.com/alwinsanil' />
+            <SkillCard name='Git' url='https://git-scm.com/' />
+            <SkillCard name='Tailwind CSS' url='https://tailwindcss.com/' />
+            <SkillCard name='Databases' url='' />
+            <SkillCard name='Power BI' url='https://powerbi.microsoft.com/en-us/' />
+            <SkillCard name='Tableau' url='https://www.tableau.com/' />
         </div>
     </div>
   )
