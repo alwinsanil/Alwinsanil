8b191e498fc16ed30e625e6ee581dd8b5040afb6
alwinsanil <alwinsanil@gmail.com>
2024-05-20 13:12:43 +0400

    exp update

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -6,16 +6,25 @@ import {MdWork} from 'react-icons/md'
 
 const Experience = () => {
   return (
-    <div id='experience' className='wrapper'>
-        <Title text='Experience' icon={<MdWork />} />
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
-            <ExpCard name='Alderaa.com | Freelance' desc='Web Developer | 2022' icon={<SiReactivex className='text-2xl' />} />
-            {/* <ExpCard name='ABC' desc='XYZ' icon={<SiReactivex className='text-2xl' />} />
+    <div id="experience" className="wrapper">
+      <Title text="Experience" icon={<MdWork />} />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <ExpCard
+          name="Lindner Prater | Internship"
+          desc="Software Developer | July 2023 - Present"
+          icon={<SiReactivex className="text-2xl" />}
+        />
+        <ExpCard
+          name="Alderaa.com | Freelance"
+          desc="Web Developer | December 2022 - February 2023"
+          icon={<SiReactivex className="text-2xl" />}
+        />
+        {/* <ExpCard name='ABC' desc='XYZ' icon={<SiReactivex className='text-2xl' />} />
             <ExpCard name='DEF' desc='YJK' icon={<SiReactivex className='text-2xl' />} />
             <ExpCard name='HIJ' desc='LMN' icon={<SiReactivex className='text-2xl' />} /> */}
-        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Experience
