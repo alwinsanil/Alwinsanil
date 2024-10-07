9a9e0f20e9c4d9deac4b845e5d91c30608b184ed
alwinsanil <alwinsanil@gmail.com>
2024-10-07 12:03:15 +0400

    Oct 24

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,24 +3,32 @@ import profilePic from '../public/img/profilepic.jpg'
 
 const Banner = () => {
   return (
-    <div className='flex flex-col md:flex-row items-center gap-10 wrapper'>
-    {/*Image*/}
-    <div className='w-40 h-40 rounded-full bg-black'>
-        <Image className='w-full h-full rounded-full border-[1px] border-white p-2 object-cover' src={profilePic} alt="ProfilePic" />
-    </div>
-    {/*Desc*/}
-    <div className='w-3/4 flex flex-col text-center md:text-start gap-2'>
-        <h1 className='text-5xl font-bold text-white'>Alwin Sanil</h1>
-        <h3 className='text-xl md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-600 to-red-600 tracking-wide'>Full Stack Developer</h3>
-        <p className='text-base tracking-wide pt-3 text-center md:text-left'>
-            With a Bachelors degree in Computer Science & Engineering, I keep learning and growing everyday. I’m
-            a quick learner who is always eager to explore new topics that interest me. I always strive to find better
-            ways to deliver high-quality results. I’m a goal-oriented person who is determined to achieve my
-            objectives by any means.
+    <div className="flex flex-col md:flex-row items-center gap-10 wrapper">
+      {/*Image*/}
+      <div className="w-40 h-40 rounded-full bg-black">
+        <Image
+          className="w-full h-full rounded-full border-[1px] border-white p-2 object-cover"
+          src={profilePic}
+          alt="ProfilePic"
+        />
+      </div>
+      {/*Desc*/}
+      <div className="w-3/4 flex flex-col text-center md:text-start gap-2">
+        <h1 className="text-5xl font-bold text-white">Alwin Sanil</h1>
+        <h3 className="text-xl md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-600 to-red-600 tracking-wide">
+          Full Stack Developer
+        </h3>
+        <p className="text-base tracking-wide pt-3 text-center md:text-left">
+          Aspiring software developer with a strong foundation in Computer
+          Science, seeking to leverage my skills in Python, front-end
+          technologies, and data processing to contribute to highly innovative
+          projects. I am dedicated to continuously upskilling and staying
+          updated to deliver the most innovative and effective solutions to meet
+          evolving demands.
         </p>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
 export default Banner
