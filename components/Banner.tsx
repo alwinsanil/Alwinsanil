b581c9bd17b3cdba293078da46bf360249749569
alwinsanil <alwinsanil@gmail.com>
2025-09-06 10:49:29 -0300

    Sept 6 update

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -16,15 +16,16 @@ const Banner = () => {
       <div className="w-3/4 flex flex-col text-center md:text-start gap-2">
         <h1 className="text-5xl font-bold text-white">Alwin Sanil</h1>
         <h3 className="text-xl md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-600 to-red-600 tracking-wide">
-          Full Stack Developer
+          Software Developer
         </h3>
         <p className="text-base tracking-wide pt-3 text-center md:text-left">
-          Aspiring software developer with a strong foundation in Computer
-          Science, seeking to leverage my skills in Python, front-end
-          technologies, and data processing to contribute to highly innovative
-          projects. I am dedicated to continuously upskilling and staying
-          updated to deliver the most innovative and effective solutions to meet
-          evolving demands.
+          A Software Developer with a strong foundation in Computer Science and
+          hands-on experience in building web applications, CRMs, and
+          project management tools. Skilled in full-stack development using
+          modern frameworks and passionate about designing intuitive, scalable,
+          and maintainable software. Eager to contribute technical expertise,
+          creativity, and problem-solving skills to a collaborative development
+          team while continuously learning and adopting emerging technologies.
         </p>
       </div>
     </div>
